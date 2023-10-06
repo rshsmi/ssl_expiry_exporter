6ee244ecdf98c74aea41e6f58f02e512a847fd61
rshsm <arashsami@live.com>
2023-10-06 21:53:49 +0100

    read certificate from file or disk

--- main.go
+++ main.go
@@ -3,73 +3,93 @@ package main
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	// "os"
-	// "time"
-
-	// "github.com/prometheus/client_golang/prometheus"
-    // "github.com/prometheus/client_golang/prometheus/promhttp"
+	"os"
+	"time"
 )
 
+var source = flag.String("source", "", "Specify 'web' to read from web server or 'file' to read from disk")
+var path = flag.String("path", "", "The URL or file path to the PEM file")
 
 func main() {
+	flag.Parse()
 
-    // Running local http server to serve multiple x509 certificate stored in a file 
-    url := "http://localhost:8080/ca.pem"
-    // url := os.Args[1]
-    // fmt.Println(url)
+	if *source == "" || *path == "" {
+		log.Fatal("Both source and path flags are required")
+	}
 
-	response, err := http.Get(url)
+	var pemData []byte
+	var err error
+
+	switch *source {
+	case "web":
+		pemData, err = readFromWeb(*path)
+	case "file":
+		pemData, err = readFromFile(*path)
+	default:
+		log.Fatal("Invalid source. Specify 'web' or 'file'.")
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer response.Body.Close()
 
-	responseData, err := ioutil.ReadAll(response.Body)
+	pemBlocks, err := parsePEMBlocks(pemData)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	responseString := string(responseData)
-
-	// fmt.Println(responseString)
-
-	var pemData = []byte(responseString)
-	var blocks [][]byte
-	rest := pemData
-	for {
-		var block *pem.Block
-		block, rest = pem.Decode(rest)
-		if block == nil {
-			fmt.Printf("Error: PEM not parsed\n")
-			break
-		}
-		blocks = append(blocks, block.Bytes)
-		if len(rest) == 0 {
-			break
-		}
-	}
-	for _, block := range blocks {
+	for _, block := range pemBlocks {
 		cert, err := x509.ParseCertificate(block)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
-		uts := cert.NotAfter.Unix()
-		// fmt.Println("Certificate:")
-		fmt.Printf("\tSubject: %+v\n", cert.Issuer)
-		fmt.Printf("\tNotafter %+v\n", cert.NotAfter)
-		fmt.Println("\tNotAfterUnix:", uts)
-		//fmt.Println("\tNotAfterUnix:", cert.NotAfter.Unix())
+		printCertDetails(cert)
+	}
+}
 
-		// fmt.Printf("\tDNS Names: %+v\n", cert.DNSNames)
-		// fmt.Printf("\tEmailAddresses: %+v\n", cert.EmailAddresses)
-		// fmt.Printf("\tIPAddresses: %+v\n", cert.IPAddresses)
+func readFromWeb(url string) ([]byte, error) {
+	client := &http.Client{
+		Timeout: time.Second * 10,
 	}
 
-// prom exporter
+	response, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
 
+	return io.ReadAll(response.Body)
+}
+
+func readFromFile(filePath string) ([]byte, error) {
+	return ioutil.ReadFile(filePath)
+}
+
+func parsePEMBlocks(data []byte) ([][]byte, error) {
+	var blocks [][]byte
+	rest := data
+	for {
+		block, rest := pem.Decode(rest)
+		if block == nil {
+			if len(rest) > 0 {
+				return nil, fmt.Errorf("remainder of data contains invalid PEM block: %s", string(rest))
+			}
+			break
+		}
+		blocks = append(blocks, block.Bytes)
+	}
+	return blocks, nil
+}
+
+func printCertDetails(cert *x509.Certificate) {
+	fmt.Printf("\tSubject: %+v\n", cert.Issuer)
+	fmt.Printf("\tNotafter %+v\n", cert.NotAfter)
+	fmt.Printf("\tNotAfterUnix: %d\n", cert.NotAfter.Unix())
 }
